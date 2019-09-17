30b232c2408697128a6d7251dc61c651695dbbf4
oliburt <oliburt@gmail.com>
2019-09-17 17:08:04 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Add your code here
+configurationObject = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify({
+        dogName: "Rover",
+        dogBreed: "Westy",
+    }),
+}
+fetch("http://localhost:3000/dogs", configurationObject)
+    .then(response => response.json())
+    .then(object => console.log(object))
+    .catch(error => {
+        alert("Bad things")
+        console.log(error.message)
+    })
+
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+        method: 'POST',
+        headers: {
+            "Content-Type": 'application/json',
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name,
+            email,
+        })
+    })
+        .then(resp => resp.json())
+        .then(obj => {
+            return document.querySelector('body').innerHTML = obj.id
+        })
+        .catch(error => {
+            const body = document.querySelector('body')
+            const p = document.createElement('p')
+            p.innerHTML = 'Unauthorized Access'
+            return body.appendChild(p)
+        })
+}
