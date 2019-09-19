e273ba76083306c9fce5cf866048ed267ec84f16
github username <aude53@gmail.com>
2019-09-19 11:08:40 +0100

    Done.

--- index.js
+++ index.js
@@ -1,29 +1,4 @@
 // // Add your code here
-// let formData = {
-//   dogName: "Byron",
-//   dogBreed: "Poodle"
-// };
-
-// let configOb = {
-//   method: "POST",
-//   headers: {
-//     "Content-Type": "application/json",
-//     Accept: "application/json"
-//   },
-//   body: JSON.stringify(formData)
-// };
-// fetch("http://localhost:3000/dogs", configOb)
-//   .then(function(response) {
-//     return response.json();
-//   })
-//   .then(function(object) {
-//     console.log(object)
-//   })
-//   .catch(function(error) {
-//     alert("Bad things! Ragnarők!")
-//     console.log(error.message)
-//   })
-
 function submitData(userName, userEmail) {
   const bodyTag = document.querySelector("body");
   return fetch("http://localhost:3000/users", {
