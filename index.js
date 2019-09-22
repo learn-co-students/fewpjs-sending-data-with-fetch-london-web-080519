47ec70d6d26e588f55a775b52f09e46e34aa4f64
tegansims <github email address>
2019-09-22 10:52:16 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+function submitData(username, useremail){
+    let bodyTag = document.querySelector("body");
+
+    let formData = {
+        name: username,
+        email: useremail
+      };
+       
+      let configObj = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+      };
+       
+      return fetch("http://localhost:3000/users", configObj)
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(object) {
+          bodyTag.innerHTML = object.id;
+        })
+        .catch(function(error) {
+          alert("Unauthorized Access");
+          console.log(error.message);
+          bodyTag.innerHTML = "Unauthorized Access"
+        });
+}
